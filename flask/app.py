8b5cd8738af97815bb8c10e5b43a71ce9fa49787
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2024-01-14 17:00:41 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('set_username')
+def handle_set_username(username):
+    print(f'Username set: {username}')
+
+@socketio.on('send_message')
+def handle_send_message(data):
+    message = data.get('message', '')
+    username = data.get('username', 'example_user')
+    print(f'Received message from {username}: {message}')
+    emit('receive_message', {'username': username, 'message': message}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
